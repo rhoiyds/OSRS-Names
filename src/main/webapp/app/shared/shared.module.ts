88ebdd430ef7e96f0d1f50b1a83c462f16b29e01
Roy Porter <royporter7@gmail.com>
2019-12-05 18:45:27 +0900

    Restyling listings table, adding dummy user profile picture and rating

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RsnsalesSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective } from './';
 import { ListingOfferComponent } from './components/listing-offer/listing-offer.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 @NgModule({
   imports: [RsnsalesSharedCommonModule],
-  declarations: [JhiLoginModalComponent, HasAnyAuthorityDirective, ListingOfferComponent],
+  declarations: [JhiLoginModalComponent, HasAnyAuthorityDirective, ListingOfferComponent, UserProfileComponent],
   entryComponents: [JhiLoginModalComponent],
-  exports: [RsnsalesSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective, ListingOfferComponent],
+  exports: [RsnsalesSharedCommonModule, JhiLoginModalComponent, HasAnyAuthorityDirective, ListingOfferComponent, UserProfileComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class RsnsalesSharedModule {
