ea2b544bab4f230dfacf97175e323f93c2450027
Roy Porter <royporter7@gmail.com>
2019-12-06 18:53:56 +0900

    Partial completion of currency pipe

---
+++ src/main/webapp/app/shared/osrs-currency.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'osrsCurrency' })
+export class OsrsCurrencyPipe implements PipeTransform {
+  transform(value: number): string {
+    if (value >= 1000000000) {
+      return value / 1000000000 + 'b';
+    }
+    if (value >= 1000000) {
+      return value / 1000000 + 'm';
+    }
+    if (value >= 1000) {
+      return value / 1000 + 'k';
+    }
+    return value.toString();
+  }
+}
