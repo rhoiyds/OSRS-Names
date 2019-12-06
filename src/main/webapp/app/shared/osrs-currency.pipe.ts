54d5d21595896f4b13de57a1925fa78c0bdf39ad
Roy Porter <royporter7@gmail.com>
2019-12-06 23:38:45 +0900

    Modifying currency pipe behavior for billions currency, fixing loading/mapping of tags on listing entity and styling tags

--- src/main/webapp/app/shared/osrs-currency.pipe.ts
+++ src/main/webapp/app/shared/osrs-currency.pipe.ts
@@ -3,8 +3,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'osrsCurrency' })
 export class OsrsCurrencyPipe implements PipeTransform {
   transform(value: number): string {
-    if (value >= 1000000000) {
-      return Math.floor(value / 1000000000) + 'b';
+    if (value >= 1000000000000) {
+      return (value / 1000000000000).toFixed(1) + 'b';
     }
     if (value >= 1000000) {
       return Math.floor(value / 1000000) + 'm';
