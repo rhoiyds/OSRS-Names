636aef4fe7e54ee262d14dcbbddcf7a3189b8884
Roy Porter <royporter7@gmail.com>
2020-01-20 18:40:27 +0900

    Properly getting paypal id configuration using ApplicationProperties

--- src/main/java/io/github/jhipster/application/config/ApplicationProperties.java
+++ src/main/java/io/github/jhipster/application/config/ApplicationProperties.java
@@ -11,4 +11,53 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
 
+    private final PayPal payPal = new PayPal();
+
+    public PayPal getPayPal() {
+        return payPal;
+    }
+    
+    public static class PayPal {
+
+        private String clientId = "";
+
+        private String secretId = "";
+
+        private String baseUrl = "";
+
+        private String webUrl = "";
+
+        public String getClientId() {
+            return clientId;
+        }
+
+        public void setClientId(String clientId) {
+            this.clientId = clientId;
+        }
+
+        public String getSecretId() {
+            return secretId;
+        }
+
+        public void setSecretId(String secretId) {
+            this.secretId = secretId;
+        }
+
+        public String getWebUrl() {
+            return webUrl;
+        }
+
+        public void setWebUrl(String webUrl) {
+            this.webUrl = webUrl;
+        }
+
+        public String getBaseUrl() {
+            return baseUrl;
+        }
+
+        public void setBaseUrl(String baseUrl) {
+            this.baseUrl = baseUrl;
+        }
+    }
+
 }
