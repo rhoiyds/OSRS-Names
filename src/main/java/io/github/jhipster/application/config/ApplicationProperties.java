d9422c1e54354ea605d03fd9b42003098e27291c
Roy Porter <royporter7@gmail.com>
2020-01-27 20:29:55 +0900

    Create product working, list products not deserializing

--- src/main/java/io/github/jhipster/application/config/ApplicationProperties.java
+++ src/main/java/io/github/jhipster/application/config/ApplicationProperties.java
@@ -1,5 +1,8 @@
 package io.github.jhipster.application.config;
 
+import com.paypal.http.annotations.Model;
+import com.paypal.http.annotations.SerializedName;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
@@ -16,8 +19,8 @@ public class ApplicationProperties {
     public PayPal getPayPal() {
         return payPal;
     }
-    
-    public static class PayPal {
+
+    public static class Credentials {
 
         private String clientId = "";
 
@@ -58,6 +61,133 @@ public class ApplicationProperties {
         public void setBaseUrl(String baseUrl) {
             this.baseUrl = baseUrl;
         }
+    }
+
+    @Model
+    public static class Product {
+
+        @SerializedName("id")
+        private String id;
+
+        @SerializedName("name")
+        private String name = "";
+
+        @SerializedName("description")
+        private String description = "";
+
+        @SerializedName("type")
+        private String type = "";
+
+        @SerializedName("category")
+        private String category = "";
+
+        @SerializedName("image_url")
+        private String imageUrl = "";
+
+        @SerializedName("home_url")
+        private String homeUrl = "";
+
+        public Product() {}
+
+        public String getId() {
+            return id;
+        }
+
+        public Product setId(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Product setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public Product setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public Product setType(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public String getCategory() {
+            return category;
+        }
+
+        public Product setCategory(String category) {
+            this.category = category;
+            return this;
+        }
+
+        public String getImageUrl() {
+            return imageUrl;
+        }
+
+        public Product setImageUrl(String imageUrl) {
+            this.imageUrl = imageUrl;
+            return this;
+        }
+
+        public String getHomeUrl() {
+            return homeUrl;
+        }
+
+        public Product setHomeUrl(String homeUrl) {
+            this.homeUrl = homeUrl;
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return "Product{" +
+                "id=" + getId() +
+                ", name='" + getName() + "'" +
+                ", description='" + getDescription() + "'" +
+                ", type='" + getType() + "'" +
+                ", category='" + getCategory() + "'" +
+                ", homeUrl='" + getHomeUrl() + "'" +
+                ", imageUrl='" + getImageUrl() + "'" +
+                "}";
+        }
+    }
+
+    public static class PayPal {
+
+        private Product product;
+
+        private Credentials credentials;
+
+        public Product getProduct() {
+            return product;
+        }
+
+        public void setProduct(Product product) {
+            this.product = product;
+        }
+
+        public Credentials getCredentials() {
+            return credentials;
+        }
+
+        public void setCredentials(Credentials credentials) {
+            this.credentials = credentials;
+        }
+
     }
 
 }
--- src/main/java/io/github/jhipster/application/service/PayPalClientService.java
+++ src/main/java/io/github/jhipster/application/service/PayPalClientService.java
@@ -1,19 +1,38 @@
 package io.github.jhipster.application.service;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.paypal.core.PayPalEnvironment;
 import com.paypal.core.PayPalHttpClient;
+import com.paypal.http.HttpRequest;
+import com.paypal.http.HttpResponse;
+import com.paypal.http.annotations.ListOf;
+import com.paypal.http.annotations.Model;
+import com.paypal.http.annotations.SerializedName;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import io.github.jhipster.application.config.ApplicationProperties;
+import io.github.jhipster.application.config.ApplicationProperties.Product;
 
-@Service
-public class PayPalClientService {
+@Component
+public class PayPalClientService implements InitializingBean {
+
+  private final Logger log = LoggerFactory.getLogger(PayPalClientService.class);
 
   /**
    *Set up the PayPal Java SDK environment with PayPal access credentials.  
    */
   public PayPalEnvironment environment;
+
+  private ApplicationProperties applicationProperties;
 
   /**
    *PayPal HTTP client instance with environment that has access
@@ -31,12 +50,131 @@ public class PayPalClientService {
   }
 
   public PayPalClientService(ApplicationProperties applicationProperties) {
+    this.applicationProperties = applicationProperties;
     this.environment = new PayPalEnvironment(
-      applicationProperties.getPayPal().getClientId(), 
-      applicationProperties.getPayPal().getSecretId(),
-      applicationProperties.getPayPal().getBaseUrl(), 
-      applicationProperties.getPayPal().getWebUrl()
+      applicationProperties.getPayPal().getCredentials().getClientId(), 
+      applicationProperties.getPayPal().getCredentials().getSecretId(),
+      applicationProperties.getPayPal().getCredentials().getBaseUrl(), 
+      applicationProperties.getPayPal().getCredentials().getWebUrl()
       );
     this.client = new PayPalHttpClient(this.environment);
   }
+
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    log.debug("PayPalClientService initialized and running products init");
+    // HttpResponse<Product> response = this.client.execute(new ProductsGetRequest());
+    Product product = this.applicationProperties.getPayPal().getProduct();
+
+    // Succesfull creation of product on PayPal
+    // ProductsCreateRequest productCreateRequest = new ProductsCreateRequest();
+    // productCreateRequest.requestBody(product);
+    // productCreateRequest.contentType("application/json");
+    // productCreateRequest.prefer("prefer=representation");
+    // HttpResponse<Product> response = this.client.execute(productCreateRequest);
+
+    ProductsGetRequest productGetRequest = new ProductsGetRequest();
+    productGetRequest.contentType("application/json");
+    HttpResponse<ProductListResponse> response = this.client.execute(productGetRequest);
+
+    log.debug("Resulting call {} {}", response.statusCode(), response.result());
+  }
+  
+  public class ProductsGetRequest extends HttpRequest<ProductListResponse> {
+
+    public ProductsGetRequest() {
+        super("/v1/catalogs/products", "GET", ProductListResponse.class);
+        header("Content-Type", "application/json");
+    }
+    public ProductsGetRequest authorization(String authorization) {
+        header("Authorization", String.valueOf(authorization));
+        return this;
+    }
+
+    public ProductsGetRequest contentType(String contentType) {
+        header("Content-Type", String.valueOf(contentType));
+        return this;
+    }
+
+}
+
+  public class ProductsCreateRequest extends HttpRequest<Product> {
+
+    public ProductsCreateRequest() {
+        super("/v1/catalogs/products", "POST", Product.class);
+        header("Content-Type", "application/json");
+    }
+    public ProductsCreateRequest authorization(String authorization) {
+        header("Authorization", String.valueOf(authorization));
+        return this;
+    }
+
+    public ProductsCreateRequest contentType(String contentType) {
+        header("Content-Type", String.valueOf(contentType));
+        return this;
+    }
+
+    public ProductsCreateRequest requestBody(Product product) {
+      super.requestBody(product);
+      return this;
+    }
+
+    public ProductsCreateRequest prefer(String prefer) {
+      header("Prefer", String.valueOf(prefer));
+      return this;
+    }
+
+}
+
+    @Model
+    @ListOf
+    public static class ProductListResponse {
+
+        // @SerializedName("total_items")
+        // private int totalItems = 0;
+
+        // @SerializedName("total_pages")
+        // private int totalPages = 0;
+
+        @SerializedName("products")
+        private PayPalProductList products;
+
+        public ProductListResponse() {}
+
+        // public int getTotalItems() {
+        //     return totalItems;
+        // }
+
+        // public ProductListResponse setTotalItems(int totalItems) {
+        //     this.totalItems = totalItems;
+        //     return this;
+        // }
+
+        // public int getTotalPages() {
+        //     return totalPages;
+        // }
+
+        // public ProductListResponse setTotalPages(int totalPages) {
+        //     this.totalPages = totalPages;
+        //     return this;
+        // }
+
+        public PayPalProductList getProducts() {
+            return products;
+        }
+
+        public ProductListResponse setProducts(PayPalProductList products) {
+            this.products = products;
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return "ProductListResponse{" +
+                // "totalItems=" + getTotalItems() +
+                // ", totalPages='" + getTotalPages() + "'" +
+                "}";
+        }
+    }
+  
 }
