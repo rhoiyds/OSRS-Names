157ba1030c26ebb69feebdca61b04f70a6fea35b
Roy Porter <royporter7@gmail.com>
2020-02-19 09:38:32 +0900

    Fixing email showing in production bug, and toString on blank value amount bug

--- src/main/java/io/github/jhipster/application/domain/User.java
+++ src/main/java/io/github/jhipster/application/domain/User.java
@@ -54,6 +54,7 @@ public class User extends AbstractAuditingEntity implements Serializable {
     @Column(name = "last_name", length = 50)
     private String lastName;
 
+    @JsonIgnore
     @Email
     @Size(min = 5, max = 254)
     @Column(length = 254, unique = true)
--- src/main/webapp/app/shared/osrs-currency.pipe.ts
+++ src/main/webapp/app/shared/osrs-currency.pipe.ts
@@ -3,6 +3,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'osrsCurrency' })
 export class OsrsCurrencyPipe implements PipeTransform {
   transform(value: number): string {
+    if (!value) {
+      return '';
+    }
     if (value >= 1000000000000) {
       return (value / 1000000000000).toFixed(1) + 'b';
     }
